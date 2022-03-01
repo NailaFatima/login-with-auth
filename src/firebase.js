b7af41aa3f5c422a173498ea7a291be3bcb8e46a
Naila  Syed <97286530+NailaFatima@users.noreply.github.com>
2022-03-02 02:37:14 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8vbQ87qRTiiXuPVMtbLeOWMHYSwuswTc",
-  authDomain: "login-react-e6e79.firebaseapp.com",
-  projectId: "login-react-e6e79",
-  storageBucket: "login-react-e6e79.appspot.com",
-  messagingSenderId: "63590643851",
-  appId: "1:63590643851:web:03eab350c8b561cb83fc8e"
+  apiKey: "<<YOURAPIKEY>>",
+  authDomain: "<<YOURAUTHDOMAIN>>",
+  projectId: "<<YOURPROJECTID>>",
+  storageBucket: "<<YOURSTORAGEBUCKET>>",
+  messagingSenderId: "<<YOURMESSAGINGSENDERID>>",
+  appId: "<<YOURAPPID>>"
 };
 
 // Initialize Firebase
